c59f1e21771e17a2f1a4c5e20eaa90eeab69ce14
Bipisha <paudelbipisha4@gmail.com>
2026-05-17 16:07:48 +0545

    singleblog changes

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import BlogForm from './components/BlogForm'
+import singleBlog from './components/singleBlog'
 
 const LoginView = ({ user, username, password, setUsername, setPassword, handleLogin }) => {
   if (user) return <Navigate to="/" />
@@ -45,28 +46,6 @@ const BlogList = ({ blogs }) => (
   </div>
 )
 
-const SingleBlog = ({ blogs, user, handleLike, handleDelete }) => {
-  const navigate = useNavigate()
-  const { id } = useParams()
-  const blog = blogs.find(b => b.id === id)
-
-  if (!blog) return <Navigate to="/" />
-
-  return (
-    <div>
-      <h2>{blog.title}</h2>
-      <p>{blog.url}</p>
-      <p>
-        likes: {blog.likes}
-        {user && <button onClick={() => handleLike(blog)}>like</button>}
-      </p>
-      <p>added by {blog.user?.name}</p>
-      {user && String(blog.user?.id || blog.user) === String(user.id) && (
-        <button onClick={() => handleDelete(blog, navigate)}>remove</button>
-      )}
-    </div>
-  )
-}
 
 const CreateBlog = ({ user, addBlog }) => {
   const navigate = useNavigate()
---
+++ part5/bloglist-frontend/src/components/singleBlog.jsx
@@ -0,0 +1,28 @@
+import { useParams, useNavigate, Navigate } from "react-router-dom";
+
+const singleBlog = ({blogs, user, handleLike, handleDelete}) => {
+  const navigate = useNavigate()
+  const {id} = useParams()
+
+  const blog = blogs.find(b => b.id === id)
+
+  if(!blog) return <Navigate to="/" />
+
+  const isCreator = user && String(blog.user?.id || blog.user) === String(user.id)
+
+  return(
+    <div data-testid="single-blog">
+      <h2>{blog.title}</h2>
+      <p>{blog.url}</p>
+      <p>
+        likes: <span data-testid="likes">{blog.likes}</span>
+      </p>
+      <p>added by {blog.user?.name}</p>
+      {isCreator && (
+        <button onClick = {() => handleDelete(blog, navigate)}>remove</button>
+      )}
+    </div>
+  )
+}
+
+export default singleBlog
