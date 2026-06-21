a1c9e3ed9e1ee63ad47b08f8ce7d092c1bf43043
Bipisha <paudelbipisha4@gmail.com>
2026-06-21 20:59:02 +0545

    6.16 to 6.19

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -1,32 +1,67 @@
+import { useQuery, useMutation, useQueryClient} from '@tanstack/react-query'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { getAnecdotes, createAnecdote, updateAnecdote } from './requests'
 
 const App = () => {
-  const handleVote = (anecdote) => {
-    console.log('vote')
+  const queryClient = useQueryClient()
+
+  const result = useQuery(
+    {
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    retry: false 
+    }
+)
+   const newAnecdoteMutation = useMutation (
+    {
+    mutationFn: createAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: ['anecdotes']
+      })
+    }
+  })
+
+  const updateAnecdoteMutation = useMutation(
+    {
+      mutationFn: updateAnecdote,
+      onSuccess: () => {
+        queryClient.invalidateQueries({queryKey: ['anecdotes']})
+      }
+    }
+  )
+
+  const addAnecdote = (content) => {
+    newAnecdoteMutation.mutate({content, votes: 0})
   }
 
-  const anecdotes = [
-    {
-      content: 'If it hurts, do it more often',
-      id: '47145',
-      votes: 0,
-    },
-  ]
+  const handleVoteCount = (anecdote) => {
+    updateAnecdoteMutation.mutate({...anecdote, votes: anecdote.votes + 1})
+  }
+
+  if(result.isPending) {
+    return <div>loading data ...</div>
+  }
+
+  if(result.isError){
+    return <div>anecdote service not available due to problems in server</div>
+  }
+
+  const anecdotes = result.data
 
   return (
     <div>
       <h3>Anecdote app</h3>
 
       <Notification />
-      <AnecdoteForm />
+      <AnecdoteForm addAnecdote={addAnecdote} />
 
       {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote)}>vote</button>
+            <button onClick={() => handleVoteCount(anecdote)}>vote</button>
           </div>
         </div>
       ))}
