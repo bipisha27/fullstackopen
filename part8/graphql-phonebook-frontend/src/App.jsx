bc9d20109e358f0ec2d9a33f282ba53dd72ba706
Bipisha <paudelbipisha4@gmail.com>
2026-06-29 21:55:55 +0545

    functionalities added

--- part8/graphql-phonebook-frontend/src/App.jsx
+++ part8/graphql-phonebook-frontend/src/App.jsx
@@ -2,16 +2,7 @@ import { gql } from "@apollo/client";
 import { useQuery } from "@apollo/client/react";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
-
-const ALL_PERSONS = gql`
-  query {
-    allPersons {
-      name
-      phone
-      id
-    }
-  }
-`;
+import { ALL_PERSONS } from "./queries";
 
 const App = () => {
   const result = useQuery(ALL_PERSONS);
