d0468942e147d0dbaa0d298e95989f8f2db3025e
Bipisha <paudelbipisha4@gmail.com>
2026-07-01 21:28:46 +0545

    completed chapter4 class activities

---
+++ part8/graphql-phonebook-backend/schema.js
@@ -0,0 +1,54 @@
+const typeDefs = `#graphql
+  type Address {
+    street: String!
+    city: String!
+  }
+
+  type Person {
+    name: String!
+    phone: String!
+    address: Address!
+    id: ID!
+  }
+
+  type User{
+    username: String!
+    friends: [String!]!
+    id: ID!
+  }
+
+  type Token {
+    value: String!
+  }
+
+  enum YesNo {
+    YES
+    NO
+  }
+
+  type Query {
+    personCount: Int!
+    allPersons(phone: YesNo): [Person!]!
+    findPerson(name: String!): Person
+    me: User
+  }
+
+  type Mutation {
+    addPerson(
+      name: String!
+      phone: String
+      street: String!
+      city: String!
+    ): Person
+
+    editNumber(name: String!, phone: String!): Person
+
+    createUser(username: String!) : User
+
+    login(username: String!, password: String!): Token
+
+    addAsFriend(name: String): User
+  }
+`;
+
+module.exports = typeDefs;
